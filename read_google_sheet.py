0d1ff6c2179a27c1607a12f4124dff630af2957c
weitsunglin <weitsunglin@igs.com.tw>
2024-02-07 13:34:09 +0800

    註解繁中

--- read_google_sheet.py
+++ read_google_sheet.py
@@ -1,16 +1,16 @@
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
-# 指定您的服务账户密钥 JSON 文件路径
+# 指定您的服務帳戶金鑰 JSON 文件路徑
 SERVICE_ACCOUNT_FILE = "/Users/tmd/Documents/WebServiceTool/TMD_BuildTool/GoogleApiSecret/ServiceCert.json"
 
-# 指定想要访问的 Google Sheets ID 和范围
+# 指定想要訪問的 Google Sheets ID 和範圍
 # 您可以在 Google Sheets URL 中找到 SHEET_ID
 SHEET_ID = '1ICIYmy5YIlE_mMYcVBTGuSsYbUwnsY8VTVuPRAyIJCQ'
-RANGE_NAME = '1.66.1!B1:B10'  # 或者您想要读取的特定范围，例如 'Sheet1!A1:E5'
+RANGE_NAME = '1.66.1!B1:B10'  # 或者您想要讀取的特定範圍，例如 'Sheet1!A1:E5'
 
 def main():
-    # 认证并构建服务
+    # 認證並構建服務
     creds = Credentials.from_service_account_file(
             SERVICE_ACCOUNT_FILE,
             scopes=['https://www.googleapis.com/auth/spreadsheets.readonly'],
@@ -18,17 +18,17 @@ def main():
 
     service = build('sheets', 'v4', credentials=creds)
 
-    # 调用 Sheets API
+    # 調用 Sheets API
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SHEET_ID,
                                 range=RANGE_NAME).execute()
     values = result.get('values', [])
 
     if not values:
-        print('No data found.')
+        print('沒有找到數據。')
     else:
         for row in values:
-            # 打印列值，根据您的需要修改
+            # 打印列值，根據您的需要修改
             print(row)
 
 if __name__ == '__main__':
