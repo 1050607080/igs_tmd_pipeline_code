b45a7298faab858e4bf6d2edafd3ed477c3de248
weitsunglin <weitsunglin@igs.com.tw>
2024-02-19 20:33:42 +0800

    test

--- Build_Release_Encode.sh
+++ Build_Release_Encode.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-echo "Start Build_Release_Encode.sh"
-/Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceEncode.py -m release -p ios -b false
-/Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateAllVersionEncode.py -m release
-read -p "Press [Enter] key to continue..."
+MODE=$1  # 获取第一个命令行参数
+echo "Start Build_Release_Encode.sh in $MODE mode"
+
+if [ "$MODE" == "debug" ]; then
+    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceEncode.py -m debug -p ios -b false
+    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateAllVersionEncode.py -m debug
+elif [ "$MODE" == "release" ]; then
+    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceEncode.py -m release -p ios -b false
+    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateAllVersionEncode.py -m release
+else
+    echo "Unknown mode: $MODE"
+    exit 1
+fi
+
+# 由于Jenkins构建通常不是交互式的，建议移除下面这行
+# read -p "Press [Enter] key to continue..."
