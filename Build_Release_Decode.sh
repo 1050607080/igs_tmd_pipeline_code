aa2cdc87117109ad9b311cb8d4ada3b302825e9a
weitsunglin <weitsunglin@igs.com.tw>
2024-02-19 21:04:26 +0800

    mac

--- Build_Release_Decode.sh
+++ Build_Release_Decode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Start Build_Release_Encode.sh"
-/Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceDecode.py -m release -p ios -b false
+/Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceDecode.py -m release -p mac -b false
 /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateAllVersionDecode.py -m release
 read -p "Press [Enter] key to continue..."
--- Build_Release_Encode.sh
+++ Build_Release_Encode.sh
@@ -3,10 +3,10 @@ MODE=$1  # 获取第一个命令行参数
 echo "Start Build_Release_Encode.sh in $MODE mode"
 
 if [ "$MODE" == "debug" ]; then
-    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceEncode.py -m debug -p ios -b false
+    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceEncode.py -m debug -p mac -b false
     /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateAllVersionEncode.py -m debug
 elif [ "$MODE" == "release" ]; then
-    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceEncode.py -m release -p ios -b false
+    /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateDownloadResourceEncode.py -m release -p mac -b false
     /Library/Frameworks/Python.framework/Versions/2.7/bin/python CreateAllVersionEncode.py -m release
 else
     echo "Unknown mode: $MODE"
