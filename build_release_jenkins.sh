5ad3bf9a2521e534a2285c74e0917c650a7d92ab
weitsunglin <weitsunglin@igs.com.tw>
2024-02-27 20:25:24 +0800

    絕對路徑

--- build_release_jenkins.sh
+++ build_release_jenkins.sh
@@ -2,6 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 /Library/Frameworks/Python.framework/Versions/2.7/bin/python ${DIR}/build_native_jenkins.py -b release
-cd ../Users/tmd/Documents/tmd/SouthPark/ClientCocos/proj.android-studio
-chmod +x gradlew
-./gradlew assembleRelease
+
+chmod +x /Users/tmd/Documents/tmd/SouthPark/ClientCocos/proj.android-studio/gradlew
+/Users/tmd/Documents/tmd/SouthPark/ClientCocos/proj.android-studio/gradlew assembleRelease
