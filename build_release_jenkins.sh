0ef13fa78136f5b8724f53ada57ac632fa10ebb6
weitsunglin <weitsunglin@igs.com.tw>
2024-02-28 21:11:11 +0800

    註解android studio code

--- build_release_jenkins.sh
+++ build_release_jenkins.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 /Library/Frameworks/Python.framework/Versions/2.7/bin/python ${DIR}/build_native_jenkins.py -b release
 
-chmod +x /Users/tmd/Documents/tmd/SouthPark/ClientCocos/proj.android-studio/gradlew
-/Users/tmd/Documents/tmd/SouthPark/ClientCocos/proj.android-studio/gradlew assembleRelease
+'''chmod +x /Users/tmd/Documents/tmd/SouthPark/ClientCocos/proj.android-studio/gradlew
+/Users/tmd/Documents/tmd/SouthPark/ClientCocos/proj.android-studio/gradlew assembleRelease'''
