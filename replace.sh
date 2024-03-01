2dac4031ab7f70727e47c98058501309ab17b3ae
weitsunglin <weitsunglin@igs.com.tw>
2024-03-01 14:25:28 +0800

    t

--- replace.sh
+++ replace.sh
@@ -1,3 +1,3 @@
 echo "Running Python script..."
-/Library/Frameworks/Python.framework/Versions/2.7/bin/python replace.py
+/Library/Frameworks/Python.framework/Versions/2.7/bin/python /Users/tmd/Documents/tmd/SouthPark/Jenkins/SyncFileTemp/replace.py
 read -p "Press Enter to continue..."
